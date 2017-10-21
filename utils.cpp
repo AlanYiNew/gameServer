4298f5dbd52d44ab448e6f584243836fb3b620f9
Alan YI <245655664@qq.com>
2017-10-20 22:37:34 -0700

    Manually upload

---
+++ utils.cpp
@@ -0,0 +1,24 @@
+//
+// Created by alan on 10/20/17.
+//
+#include "utils.h"
+int Util::randomRange(int begin, int end){
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(begin,end);
+    return distribution(generator);
+}
+
+std::pair<int,int> Util::randomPairInRange(int begin,int end){
+    std::vector<int> vec;
+    for (int i = begin; i < end;++i){
+        vec.push_back(i);
+    }
+
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(0,end-begin+1);
+    int val1 = distribution(generator);
+    std::swap(vec[val1],vec[vec.size()-1]);
+    distribution = std::uniform_int_distribution<int>(0,end-begin);
+    int val2 = distribution(generator);
+    return {vec[vec.size()-1],vec[val2]};
+};
---
+++ utils.h
@@ -0,0 +1,19 @@
+//
+// Created by alan on 10/20/17.
+//
+
+#ifndef GAMESERVER_UTILS_H
+#define GAMESERVER_UTILS_H
+#include <random>
+#include <utility>
+#include <vector>
+
+class Util{
+public:
+    int randomRange(int begin, int end);
+    std::pair<int,int> randomPairInRange(int begin,int end);
+
+};
+
+
+#endif //GAMESERVER_UTILS_H
