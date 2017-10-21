7fd5fce5d0e0c1225ceddbf4276d8ecc8750e6bd
Alan YI <245655664@qq.com>
2017-10-20 22:36:39 -0700

    Mannualy upload

---
+++ modules/Map.cpp
@@ -0,0 +1,16 @@
+//
+// Created by alan on 10/20/17.
+//
+#include "Map.h"
+
+int MapModule::getRandomMap() {
+    Util temp;
+    return temp.randomRange(0,_maps.size()-1);
+}
+
+std::pair<int,int> MapModule::randomSpawns(int mapid) {
+
+    Util temp;
+
+    return temp.randomPairInRange(0,_maps[mapid]-1);
+}
---
+++ modules/Map.h
@@ -0,0 +1,21 @@
+//
+// Created by alan on 10/20/17.
+//
+
+#ifndef GAMESERVER_MAP_H
+#define GAMESERVER_MAP_H
+#include "../utils.h"
+#include <utility>
+#include <vector>
+
+class MapModule{
+private:
+    int _num = 4;
+    std::vector<int> _maps = {4};
+
+public:
+    int getRandomMap();
+    std::pair<int,int> randomSpawns(int mapid);
+};
+
+#endif //GAMESERVER_MAP_H
